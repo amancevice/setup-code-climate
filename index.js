0daf2985a225e8ac15975b4d233010e94d65b76a
Alexander Mancevice <alexander.mancevice@hey.com>
2024-03-10 10:29:44 -0400

    clean up index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const core  = require('@actions/core');
+const core = require('@actions/core');
 
 const setup = require('./lib/setup-code-climate');
 
-const main  = async () => {
-  try {
-    await setup();
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+const main = async () => {
+  await setup().catch((e) => core.setFailed(e.message))
 };
 
 main();
